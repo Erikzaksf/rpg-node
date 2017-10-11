602e027a965e70c29421937dfce928471051ce1a
Erik Zakrzewski and Kelsey Langlois <erik@>
2017-10-11 10:47:38 -0700

    Add test for fail case of Pearl spear throw

--- spec/pearl-spec.js
+++ spec/pearl-spec.js
@@ -20,11 +20,20 @@ describe('Pearl', function() {
       expect(testPearl.abilityPoints).toEqual(20);
     });
   });
+
   describe('spearThrow', function() {
     it("removes Pearls strength from targets hp", function() {
       let testEnemy = new Character("Jasper");
       testPearl.throwSpear(testEnemy);
       expect(testEnemy.health).toEqual(92);
+    });
+
+    it("does not cast if Pearl has less than 5 AP", function() {
+      let testEnemy = new Character("Jasper");
+      testEnemy.health = 10;
+      testPearl.abilityPoints = 3;
+      expect(testPearl.throwSpear(testEnemy)).toEqual(false);
+      expect(testEnemy.health).toEqual(10);
     });
   });
 
